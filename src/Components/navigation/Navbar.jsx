041acca22f472bf7bb57b49101505e5ef567fec8
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-11-09 15:05:22 +0600

    fix the navbar

--- src/Components/navigation/Navbar.jsx
+++ src/Components/navigation/Navbar.jsx
@@ -67,9 +67,9 @@ const Navbar = () => {
 
     return (
         <div className='sticky top-0 z-50'>
-            <div className={` transition-colors duration-300 ${scrolled ? "bg-[#00000010] backdrop-blur-[12px] border-b-2 border-b-[#0000000e] " : "bg-white dark:bg-gray-900"
+            <div className={` transition-colors duration-300  ${scrolled ? "bg-[#00000010] backdrop-blur-[12px] border-b-2 border-b-[#0000000e] " : "bg-white dark:bg-gray-900"
                 }`} >
-                <div className="navbar ">
+                <div className="navbar max-w-[1500px] mx-auto ">
                     <div className="navbar-start">
                         {/* mobile dropdown */}
                         <div className="dropdown">
@@ -122,24 +122,24 @@ const Navbar = () => {
                         {user ? (
                             <div className=' flex lg:flex-col xl:flex-row items-center xl:gap-4'>
                                 <div className='flex flex-row items-center gap-6 xl:gap-4 '>
-                                    <Link to='/' className=' text-xl text-green-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
+                                    <Link to='/' className=' text-xl text-blue-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
                                         Home
                                     </Link>
-                                    <Link to='/allblogs' className=' text-xl text-green-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
+                                    <Link to='/allblogs' className=' text-xl text-blue-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
                                         All blogs
                                     </Link>
-                                    <Link to='/featuredblogs' className=' text-xl text-green-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
+                                    <Link to='/featuredblogs' className=' text-xl text-blue-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
                                         Featured Blogs
                                     </Link>
                                 </div>
                                 <div className='flex flex-row items-center gap-6 xl:gap-4'>
-                                    <Link to='/addblog' className='text-xl text-green-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
+                                    <Link to='/addblog' className='text-xl text-blue-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
                                         Add Blog
                                     </Link>
-                                    <Link to='/wishlist' className='text-xl text-green-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
+                                    <Link to='/wishlist' className='text-xl text-blue-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
                                         Wishlist
                                     </Link>
-                                    <Link to='/dashboard' className='text-xl text-green-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
+                                    <Link to='/dashboard' className='text-xl text-blue-600 rounded-md hover:text-blue-500 transition-all duration-500 font-bold'>
                                         Dashboard
                                     </Link>
                                 </div>
@@ -147,21 +147,21 @@ const Navbar = () => {
                         ) : (
                             <div className="flex flex-row items-center gap-4 px-1">
                                 <div>
-                                    <Link to='/' className='text-xl text-green-600 font-bold hover:text-blue-500 transition-all duration-500'>Home</Link>
+                                    <Link to='/' className='text-xl text-blue-600 font-bold hover:text-blue-500 transition-all duration-500'>Home</Link>
                                 </div>
                                 <div>
-                                    <Link to='/allblogs' className='text-xl text-green-600 font-bold hover:text-blue-500 transition-all duration-500'>All blogs</Link>
+                                    <Link to='/allblogs' className='text-xl text-blue-600 font-bold hover:text-blue-500 transition-all duration-500'>All blogs</Link>
                                 </div>
                                 <div>
-                                    <Link to='/featuredblogs' className='text-xl text-green-600 font-bold hover:text-blue-500 transition-all duration-500'>Featured Blogs</Link>
+                                    <Link to='/featuredblogs' className='text-xl text-blue-600 font-bold hover:text-blue-500 transition-all duration-500'>Featured Blogs</Link>
                                 </div>
-                                <Link to='/addblog' className='text-xl text-green-600 font-bold hover:text-blue-500 transition-all duration-500'>
+                                <Link to='/addblog' className='text-xl text-blue-600 font-bold hover:text-blue-500 transition-all duration-500'>
                                     Add Blog
                                 </Link>
-                                <Link to='/wishlist' className='text-xl text-green-600 font-bold hover:text-blue-500 transition-all duration-500'>
+                                <Link to='/wishlist' className='text-xl text-blue-600 font-bold hover:text-blue-500 transition-all duration-500'>
                                     Wishlist
                                 </Link>
-                                <Link to='/dashboard' className='text-xl text-green-600 font-bold hover:text-blue-500 transition-all duration-500'>
+                                <Link to='/dashboard' className='text-xl text-blue-600 font-bold hover:text-blue-500 transition-all duration-500'>
                                     Dashboard
                                 </Link>
                             </div>
