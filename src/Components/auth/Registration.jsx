65a8e18078297e9cbfa5c92440efd83184966a36
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-11-09 15:44:27 +0600

    registration page is ok

--- src/Components/auth/Registration.jsx
+++ src/Components/auth/Registration.jsx
@@ -13,7 +13,7 @@ import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 
 
 const Registration = () => {
-    const [showpas,setshowpas] = useState(false)
+    const [showpas, setshowpas] = useState(false)
     const navigate = useNavigate();
     // get api link from env file
     const API = import.meta.env.VITE_API;
@@ -23,7 +23,7 @@ const Registration = () => {
         const upperCaseCheck = /[A-Z]/.test(password);
         const lowerCaseCheck = /[a-z]/.test(password);
         const lengthCheck = password.length >= 6; // Minimum Length Check
-    
+
         if (!upperCaseCheck) {
             toast.error("Must have an Uppercase leer in the password");
             return false;
@@ -37,14 +37,14 @@ const Registration = () => {
             return false;
         }
         return true;
-        };
+    };
 
-    
+
     const handelRegistra = (event) => {
-        
+
 
         event.preventDefault();
-        
+
         const name = event.target.name.value;
         const email = event.target.email.value;
         const photoURL = event.target.PhotoURL.value;
@@ -53,10 +53,10 @@ const Registration = () => {
         if (!validatePassword(password)) {
             return;
         }
-        
+
 
         console.log(name, email, photoURL, password);
-        
+
 
         // Creating a user with Firebase Authentication
         createUserWithEmailAndPassword(auth, email, password)
@@ -71,7 +71,7 @@ const Registration = () => {
                 })
                     .then(() => {
                         console.log('Profile updated successfully');
-                        const userData ={ name, email, photoURL};
+                        const userData = { name, email, photoURL };
                         // -----------send to server
                         fetch(`${API}/userData`, {
                             method: 'POST',
@@ -80,14 +80,14 @@ const Registration = () => {
                             },
                             body: JSON.stringify(userData),
                         })
-                        .then((res) => res.json())
-                        .then((data) => {
-                            console.log(data);
-                        })
-                        .catch((error) => {
-                            console.error('Error:', error);
-                            toast.error("Error adding Blog");
-                        } );
+                            .then((res) => res.json())
+                            .then((data) => {
+                                console.log(data);
+                            })
+                            .catch((error) => {
+                                console.error('Error:', error);
+                                toast.error("Error adding Blog");
+                            });
                     })
                     .catch((error) => {
                         console.error('Error updating profile:', error.message);
@@ -99,7 +99,7 @@ const Registration = () => {
             });
 
 
-            
+
 
     };
 
@@ -107,98 +107,92 @@ const Registration = () => {
     return (
         <div>
             <div>
-                <Navbar/>
-                <div>
-                <div>
-                <h2 className="text-4xl font-bold text-center mb-2 mt-11">Registration/Signup</h2>
-                <div>
-                    <div className="hero  min-h-screen">
-                        <div className="hero-content flex-col lg:flex-row-reverse">
-                            {/* Animation */}
-                            <div className="text-center lg:text-left border-[7px] py-20 rounded-md bg-blue-50 border-blue-300">
-                                <DotLottieReact
-                                    src="https://lottie.host/f107d7b7-2e9a-463e-9698-8b965b72238f/mPsFMFWlBi.lottie"
-                                    loop
-                                    autoplay
-                                    className='w-[300px]  md:w-[600px]'
-                                />
-                                <p className="py-6 text-center">
-                                    registration for login our website
-                                </p>
-                            </div>
-                            {/* Registration */}
-                            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                                <form onSubmit={handelRegistra} className="card-body">
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Name</span>
-                                        </label>
-                                        <input
-                                            type="text"
-                                            placeholder="Name"
-                                            name="name"
-                                            className="input input-bordered"
-                                            required
-                                        />
+                {/* <Navbar /> */}
+                <div className='max-w-screen overflow-x-hidden max-h-screen overflow-y-hidden bg-[#e2dada] dark:bg-gray-900 '>
+                    <div>
+                        <div className="hero  min-h-screen">
+                            <div className="flex flex-col lg:flex-row">
+                                {/* photo */}
+                                <div className='hidden lg:flex w-[50vw] h-screen  bg-blue-300' style={{ backgroundImage: `url('https://i.ibb.co.com/mr817yZC/mountain-7775867.jpg')`, backgroundSize: 'cover' }}>
+
+                                </div>
+
+                                {/* Registration */}
+                                <div className='w-[50vw] h-screen flex flex-col items-center justify-center '>
+                                    <h2 className='text-lg lg:text-2xl font-bold text-center'>Registration on email and password</h2>
+                                    <div className=" w-[300px] lg:w-full max-w-sm shrink-0">
+                                        <form onSubmit={handelRegistra} className="card-body">
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text  text-gray-700 dark:text-gray-200">Name</span>
+                                                </label>
+                                                <input
+                                                    type="text"
+                                                    placeholder="Name"
+                                                    name="name"
+                                                    className="input input-bordered bg-white dark:bg-gray-800"
+                                                    required
+                                                />
+                                            </div>
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text text-gray-700 dark:text-gray-200">Email</span>
+                                                </label>
+                                                <input
+                                                    type="email"
+                                                    placeholder="email"
+                                                    name="email"
+                                                    className="input input-bordered bg-white dark:bg-gray-800"
+                                                    required
+                                                />
+                                            </div>
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text  text-gray-700 dark:text-gray-200">Photo-URL</span>
+                                                </label>
+                                                <input
+                                                    type="text"
+                                                    placeholder="Photo-URL"
+                                                    name="PhotoURL"
+                                                    className="input input-bordered bg-white dark:bg-gray-800"
+                                                    required
+                                                />
+                                            </div>
+                                            <div className="form-control relative">
+                                                <label className="label">
+                                                    <span className="label-text  text-gray-700 dark:text-gray-200">Password</span>
+                                                </label>
+                                                <input
+                                                    type={showpas ? 'text' : 'password'}
+                                                    placeholder="password"
+                                                    name="password"
+                                                    className="input input-bordered bg-white dark:bg-gray-800"
+                                                    required
+                                                />
+                                                <button
+                                                    onClick={() => setshowpas(!showpas)}
+                                                    type="button"
+                                                    className='btn btn-xs absolute right-4 top-12'>
+                                                    {
+                                                        showpas ? <FaEye ></FaEye> : <FaEyeLowVision ></FaEyeLowVision >
+                                                    }
+                                                </button>
+                                            </div>
+                                            <div className="form-control mt-6">
+                                                <button type="submit" className="btn btn-primary text-white">
+                                                    Register
+                                                </button>
+                                            </div>
+                                            <Link to="/Login">Or Login</Link>
+                                        </form>
                                     </div>
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Email</span>
-                                        </label>
-                                        <input
-                                            type="email"
-                                            placeholder="email"
-                                            name="email"
-                                            className="input input-bordered"
-                                            required
-                                        />
-                                    </div>
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Photo-URL</span>
-                                        </label>
-                                        <input
-                                            type="text"
-                                            placeholder="Photo-URL"
-                                            name="PhotoURL"
-                                            className="input input-bordered"
-                                            required
-                                        />
-                                    </div>
-                                    <div className="form-control relative">
-                                        <label className="label">
-                                            <span className="label-text">Password</span>
-                                        </label>
-                                        <input
-                                            type={showpas ? 'text' : 'password' } 
-                                            placeholder="password"
-                                            name="password"
-                                            className="input input-bordered"
-                                            required
-                                        />
-                                        <button 
-                                        onClick={() => setshowpas(!showpas)}
-                                        type="button"
-                                        className='btn btn-xs absolute right-4 top-12'>
-                                        {
-                                            showpas ? <FaEye ></FaEye> : <FaEyeLowVision ></FaEyeLowVision >
-                                        }
-                                        </button>
-                                    </div>
-                                    <div className="form-control mt-6">
-                                        <button type="submit" className="btn btn-primary">
-                                            Register
-                                        </button>
-                                    </div>
-                                    <Link to="/Login">Or Login</Link>
-                                </form>
+                                </div>
+
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-                </div>
-                <Footer/>
+                {/* <Footer /> */}
             </div>
         </div>
     );
